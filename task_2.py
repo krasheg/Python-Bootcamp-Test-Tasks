75a0484ff864375fef0c5307be9e10ac9685af78
INlOEtdF <krasheg@gmail.com>
2022-08-01 22:47:05 +0300

    removed the extra functionality

--- task_2.py
+++ task_2.py
@@ -1,5 +1,4 @@
 # This file contains a function that converts video to GIF and returns it abs path
-import requests
 from moviepy.editor import VideoFileClip
 import itertools
 from pathlib import Path
@@ -8,38 +7,32 @@ import os
 
 def video_to_gif(link: str, filename: str = 'TikTok Example'):
     """
-    This function downloads video from url and converts it into .gif and returns it`s abs path
+    This function converts video into .gif and returns it`s abs path
     :param link: url for video
-    :param filename: name of video
+    :param filename: name for gif
     :return: abs path to .gif file
     """
-    # check if filename has .mp4
-    if not filename.endswith('.mp4'):
-        filename += '.mp4'
-
-    # create response object
-    r = requests.get(link, stream=True)
-
-    # download started
-    with open(filename, 'wb') as f:
-        for chunk in r.iter_content(chunk_size=1024 * 1024):
-            if chunk:
-                f.write(chunk)
     # using moviepy lib for convert .mp4 to gif
-    videoclip = VideoFileClip(filename)
-    # creating a filename for .gif and try to save it
+    try:
+        videoclip = VideoFileClip(link)
+    except Exception as e:
+        print(e, "Can`t use this link")
+    # creating a filename for .gif and save it
     path = Path(filename.replace(' ', '-').split(".")[0] + ".gif")
     for i in itertools.count(1):
         new_path = path.parent / (path.stem + f'-{i}' + path.suffix)
         if not new_path.exists():
-            videoclip.write_gif(new_path, fps=15)
+            try:
+                videoclip.write_gif(new_path, fps=5)  # for faster convertation
+            except Exception as e:
+                print(e, 'Can`t convert this video to .gif')
             return os.path.abspath(new_path)
 
 
 if __name__ == "__main__":
-    link = "https://v16m-webapp.tiktokcdn-us.com/ed129ecb01ab00e202682e99f68a9288/62e7cb0d/video/tos/useast5/tos" \
-           "-useast5-pve-0068-tx/d69985b1677b4a73a584b56d604011ca/?a=1988&ch=0&cr=0&dr=0&lr=tiktok_m&cd=0%7C0%7C1%7C0" \
-           "&cv=1&br=4020&bt=2010&cs=0&ds=3&ft=ebtHKH-qMyq8ZjFl1we2N9befl7Gb&mime_type=video_mp4&qs=0&rc" \
-           "=OTU4MzU0NzVnaDpnOGg8OEBpajM5Z2c6ZmYzZTMzZzczNEAuMC9jLWBgNmExMzJfY18tYSMxX28vcjRnMGRgLS1kMS9zcw%3D%3D&l" \
-           "=20220801064449EF653E99EF32BC2EAB55 "
+    link = "https://v16-webapp.tiktok.com/ac8370e95848330ef7e530b461ea1ef3/62e87c9a/video/tos/maliva/tos-maliva-ve" \
+           "-0068c799-us/0f9740522a1c45f092da7b8eb42a0ef3/?a=1988&ch=0&cr=0&dr=0&lr=tiktok_m&cd=0%7C0%7C1%7C0&cv=1&br" \
+           "=1948&bt=974&btag=80000&cs=0&ds=3&ft=eXd.6H-oMyq8Z-MoKwe2NP0Aol7Gb&mime_type=video_mp4&qs=0&rc" \
+           "=OTk3aGk4M2U2Z2kzPDtmNEBpM3M4PDw6Zjw2PDMzZzczNEA2YWFfMGItXi0xXzU0Ml5iYSMxNWAvcjRvamJgLS1kMS9zcw%3D%3D&l" \
+           "=20220801192148010192168091065B216A "
     print(video_to_gif(link))
